152ee304705c23573084f9996ceb76df2014757c
Tropid <rdk.keller@gmail.com>
2024-09-18 16:41:23 +0200

    nvim: use mini files

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -8,7 +8,7 @@ return {
     })
 
     -- require('mini.align').setup()
-    -- require('mini.files').setup()
+    require('mini.files').setup()
     require('mini.operators').setup()
     -- require('mini.surround').setup()
     require('mini.splitjoin').setup()
