909d125ba19ca6c1e35767e7e9b0a393c2d137b6
Richard Keller <rdk.keller@gmail.com>
2024-12-18 22:52:33 +0100

    Move to oil, blink and fzf lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,40 +1,31 @@
-local border = {
-	{ "🭽", "FloatBorder" },
-	{ "▔", "FloatBorder" },
-	{ "🭾", "FloatBorder" },
-	{ "▕", "FloatBorder" },
-	{ "🭿", "FloatBorder" },
-	{ "▁", "FloatBorder" },
-	{ "🭼", "FloatBorder" },
-	{ "▏", "FloatBorder" },
-}
-
--- LSP settings (for overriding per client)
-local handlers = {
-	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
-}
-
 return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
+			"saghen/blink.cmp",
 			"williamboman/mason.nvim",
 			"folke/lazydev.nvim",
 		},
-		config = function()
+		opts = {
+			servers = {
+				lua_ls = {},
+				clangd = {},
+				jsonls = {},
+				rust_analyzer = {},
+				zls = {},
+			},
+		},
+		config = function(_, opts)
 			local lspconfig = require("lspconfig")
 			require("mason").setup()
 			require("lazydev").setup()
 
-			lspconfig.clangd.setup({ handlers = handlers })
-			lspconfig.gleam.setup({ handlers = handlers })
-			lspconfig.jsonls.setup({ handlers = handlers })
-			lspconfig.lua_ls.setup({ handlers = handlers })
-			-- lspconfig.pyright.setup({ handlers = handlers })
-			lspconfig.ruff.setup({ handlers = handlers })
-			lspconfig.rust_analyzer.setup({ handlers = handlers })
-			lspconfig.zls.setup({ handlers = handlers })
+			for server, config in pairs(opts.servers) do
+				-- passing config.capabilities to blink.cmp merges with the capabilities in your
+				-- `opts[server].capabilities, if you've defined it
+				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+				lspconfig[server].setup(config)
+			end
 		end,
 	},
 }
