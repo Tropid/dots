60c4c268c0f203e0d5d28010abc9058fd0f0f100
Richard Keller <rdk.keller@gmail.com>
2024-12-19 00:13:41 +0100

    Add snacks

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,35 +1,35 @@
 return {
-  {
-    "EdenEast/nightfox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('nightfox').setup({
-        options = {
-          transparent = true,
-        },
-      })
-      vim.cmd([[colorscheme nightfox]])
-    end,
-  },
   -- {
-  --   "folke/tokyonight.nvim",
+  --   "EdenEast/nightfox.nvim",
   --   lazy = false,
   --   priority = 1000,
-  --   opts = {
-  --     transparent = true,
-  --   },
   --   config = function()
-  --     require('tokyonight').setup({
-  --       style = "storm",
-  --       light_style = "day",
-  --       transparent = "true",
-  --       styles = {
-  --         sidebars = "transparent",
-  --         floats = "transparent",
-  --       }
+  --     require('nightfox').setup({
+  --       options = {
+  --         transparent = true,
+  --       },
   --     })
-  --     vim.cmd([[colorscheme tokyonight]])
+  --     vim.cmd([[colorscheme nightfox]])
   --   end,
   -- },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+    config = function()
+      require('tokyonight').setup({
+        style = "storm",
+        light_style = "day",
+        transparent = "true",
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        }
+      })
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
 }
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		---@type snacks.Config
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			bigfile = { enabled = true },
+			dashboard = { enabled = true },
+			indent = { enabled = true },
+			input = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			scroll = { enabled = false },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+		},
+    keys = {
+      { "<leader>z", function() Snacks.zen() end, desc = "Toggle Zen Mode" },
+      { "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+      { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+      { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+      { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+    },
+	},
+}
